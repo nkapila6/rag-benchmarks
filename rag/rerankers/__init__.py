6e4434e83ac3da01c01349cf3049ab16ba79c6be
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2025-08-20 14:50:37 +0530

    feat: added cross encoder reranker

--- rag/rerankers/__init__.py
+++ rag/rerankers/__init__.py
@@ -1,10 +1,11 @@
 from .base import BaseReranker
 from .norerank import NoReranker
 from .bi_encoder import BiEncoderReranker
+from .cross_encoder import CrossEncoderReranker
 
 __all__ = [
     "BaseReranker",
     "NoReranker",
     "BiEncoderReranker",
-    # "CrossEncoderReranker",
+    "CrossEncoderReranker",
 ] 
